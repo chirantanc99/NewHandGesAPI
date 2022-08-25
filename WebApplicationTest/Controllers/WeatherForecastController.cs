99b47edd62dbf3536b3ed068948bf60513c5180f
chirantan.chatterjee <chirantan.chatterjee@iskraemeco.com>
2022-08-25 18:07:17 +0530

    change

--- WebApplicationTest/Controllers/WeatherForecastController.cs
+++ WebApplicationTest/Controllers/WeatherForecastController.cs
@@ -7,17 +7,11 @@ namespace WebApplicationTest.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+       
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
@@ -29,10 +23,8 @@ namespace WebApplicationTest.Controllers
             //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             //})
             //.ToArray();
-            string[] arr = new string[2];
-            arr[0] = "4";
-            arr[1] = "2";
-            return Enumerable.Range(1,2).Select(index => new WeatherForecast
+            
+            return Enumerable.Range(0,1).Select(index => new WeatherForecast
             {
                 prediction = BackEnd_proj.Model.ModelFit()
             }) ;
